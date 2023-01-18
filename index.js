b28af0847de43fbce87740025f6903afeb62031b
Maksym Kachanov <m.b.kachanov@gmail.com>
2023-01-18 22:00:03 +0200

    adds shadow for result

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const buttonCards = document.querySelectorAll(".button-card");
 
 buttonCards.forEach(button => {
     button.addEventListener("click", event => {
-        mainCard.classList.add("flipped");
-        mainCard.style.backgroundColor = Math.random() > 0.5 ? "black" : "white";
+        let randomColor = Math.random() > 0.5 ? "black" : "white"
+        let guessResult = event.target.id == randomColor ? "shadow-success" : "shadow-failure"
+        console.log(event.target.id == randomColor, guessResult)
+        mainCard.classList.add("flipped", guessResult);
+        mainCard.style.backgroundColor = randomColor;
         setTimeout(() => {
-            mainCard.classList.remove("flipped");
+            mainCard.classList.remove("flipped", guessResult);
             mainCard.style.backgroundColor = "#bfa6ea";
         }, 500);
     });
